b958f9b8b7dddc3173c14aa9ae07cf0f8a1ed6f9
nack1400 <nack1400@naver.com>
2022-07-16 00:03:37 +0900

    Add basic : user agent

---
+++ 5_user_agent.py
@@ -0,0 +1,8 @@
+import requests
+url = "https://nack1400.tistory.com/"
+headers = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36"}
+res = requests.get(url, headers=headers)
+res.raise_for_status()
+
+with open("mycoding.html", "w", encoding="utf8") as f:
+  f.write(res.text)
