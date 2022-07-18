2bc9c2fabdb6256dd6ab7566e1e1d62cbab121c7
nack1400 <nack1400@naver.com>
2022-07-18 10:42:34 +0900

    Add basic : bs naver webtoon2

--- 6_bs4.py
+++ 6_bs4.py
@@ -18,6 +18,24 @@ soup = BeautifulSoup(res.text, "lxml")
 # print(soup.find("a", attrs={"class" : "Nbtn_upload"})) # class = "Nbtn_upload"인 a element를 찾아줘
 # print(soup.find(attrs={"class" : "Nbtn_upload"})) # class = "Nbtn_upload"인 어떤 element를 찾아줘
 
-print(soup.find("li", attrs={"class": "rank01"}))
+# print(soup.find("li", attrs={"class": "rank01"}))
 rank1 = soup.find("li", attrs={"class": "rank01"})
-print(rank1.a)
+# print(rank1.a.get_text())
+# print(rank1.next_sibling)
+# rank2 = rank1.next_sibling.next_sibling
+# rank3 = rank2.next_sibling.next_sibling
+# print(rank3.a.get_text())
+# rank2 = rank3.previous_sibling.previous_sibling
+# print(rank2.a.get_text())
+# print(rank1.parent)
+# rank2 = rank1.find_next_sibling("li")
+# print(rank2.a.get_text())
+# rank3 = rank2.find_next_sibling("li")
+# print(rank3.a.get_text())
+# rank2 = rank3.find_previous_sibling("li")
+# print(rank2.a.get_text())
+
+# print(rank1.find_next_siblings("li"))
+
+webtoon = soup.find("a", text="독립일기-시즌2 82화 추억 팔이")
+print(webtoon)
